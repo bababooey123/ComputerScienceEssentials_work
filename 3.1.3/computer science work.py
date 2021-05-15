5b0f974c8211714831b048df7eba7bac2cca7c5e
bababooey123 <81046653+bababooey123@users.noreply.github.com>
2021-05-15 13:15:07 -0500

    Update computer science work.py

--- 3.1.3/computer science work.py
+++ 3.1.3/computer science work.py
@@ -3,28 +3,35 @@ fries = 0.0
 sandwich = 0.0
 drink = 0.0
 ketchup = 0.0
+ree = []
 sandwich_order = input("would you like a beef sandwich [6.25] a chicken sandwich [5.25] or a tofu sandwich[5.75]")
 if sandwich_order == ("beef"):
     sandwich = 6.25
     coupon += 1
+    ree.append("beef")
 elif sandwich_order == ("chicken"):
     sandwich = 5.25
     coupon += 1
+    ree.append("chicken")
 elif sandwich_order == ("tofu"):
     sandwich = 5.75
     coupon += 1
+    ree.append("tofu")
 print(sandwich_order + " " + str(sandwich))
 # drink time wooooooooo
 drink_order = input("Large drink(2.25) medium drink(1.75) small drink (1.00) or no?")
 if drink_order == ("large"):
     drink = 2.25
     coupon += 1
+    ree.append("Large drink")
 elif drink_order == ("medium"):
     drink = 1.75
     coupon += 1
+    ree.append("Medium drink")
 elif drink_order == ("small"):
     drink = 1.00
     coupon += 1
+    ree.append("Small drink")
 elif drink_order == ("no"):
     drink = 0.0
 # fries woop woop
@@ -34,23 +41,29 @@ if fry_order == ("no"):
 elif fry_order == ("medium"):
     fries = 1.50
     coupon += 1
+    ree.append("Medium fries")
 elif fry_order == ("small"):
     megasize = input("would you like to megasize that?")
     if megasize == ("yes"):
         fries = 2.00
         coupon += 1
+        ree.append("Large fries")
         print("succesfully MEGASIZED")
     elif megasize == "no":
         fries = 1.00
         coupon += 1
+        ree.append("Small fries")
 elif fry_order == "large":
     fries = 2.00
     coupon += 1
+    ree.append("Large fries")
     # ketchup time
 ketchup_packets = input("how many ketchup packets would you like? (0.25) each")
 ketchup = (float(ketchup_packets) * 0.25)
+ree.append(ketchup_packets)
 total = (sandwich + drink + fries + ketchup)
 if coupon == 3:
     total -= 1
     print("you get a discount")
-print("your order is " + str(sandwich_order) + " sandwich " + str(drink_order) + " drink " + str(fry_order) + " fries " + str(ketchup_packets) + " ketchup packet your total is " + str(total))
+print("your order is " + str(ree))
+print("your total is " + str(total))
