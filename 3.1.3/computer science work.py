4bdcf40f57bb143f6ff648d6448e605d13758c30
bababooey123 <81046653+bababooey123@users.noreply.github.com>
2021-05-15 14:19:15 -0500

    Update computer science work.py

--- 3.1.3/computer science work.py
+++ 3.1.3/computer science work.py
@@ -4,65 +4,83 @@ sandwich = 0.0
 drink = 0.0
 ketchup = 0.0
 ree = []
-sandwich_order = input("would you like a beef sandwich [6.25] a chicken sandwich [5.25] or a tofu sandwich[5.75]")
-if sandwich_order == ("beef"):
-    sandwich = 6.25
-    coupon += 1
-    ree.append("beef")
-elif sandwich_order == ("chicken"):
-    sandwich = 5.25
-    coupon += 1
-    ree.append("chicken")
-elif sandwich_order == ("tofu"):
-    sandwich = 5.75
-    coupon += 1
-    ree.append("tofu")
-print(sandwich_order + " " + str(sandwich))
-# drink time wooooooooo
-drink_order = input("Large drink(2.25) medium drink(1.75) small drink (1.00) or no?")
-if drink_order == ("large"):
-    drink = 2.25
-    coupon += 1
-    ree.append("Large drink")
-elif drink_order == ("medium"):
-    drink = 1.75
-    coupon += 1
-    ree.append("Medium drink")
-elif drink_order == ("small"):
-    drink = 1.00
-    coupon += 1
-    ree.append("Small drink")
-elif drink_order == ("no"):
-    drink = 0.0
-# fries woop woop
-fry_order = input("large fries(2.00) medium fries(1.50) small fries(1.00)")
-if fry_order == ("no"):
-    fries = 0.0
-elif fry_order == ("medium"):
-    fries = 1.50
-    coupon += 1
-    ree.append("Medium fries")
-elif fry_order == ("small"):
-    megasize = input("would you like to megasize that?")
-    if megasize == ("yes"):
-        fries = 2.00
-        coupon += 1
-        ree.append("Large fries")
-        print("succesfully MEGASIZED")
-    elif megasize == "no":
-        fries = 1.00
-        coupon += 1
-        ree.append("Small fries")
-elif fry_order == "large":
-    fries = 2.00
-    coupon += 1
-    ree.append("Large fries")
-    # ketchup time
-ketchup_packets = input("how many ketchup packets would you like? (0.25) each")
-ketchup = (float(ketchup_packets) * 0.25)
-ree.append(ketchup_packets)
-total = (sandwich + drink + fries + ketchup)
-if coupon == 3:
+price = []
+coupon = []
+def memes():
+  sandwich_order = input("would you like a beef sandwich [6.25] a chicken sandwich [5.25] or a tofu sandwich[5.75]")
+  if sandwich_order == ("beef"):
+      sandwich = 6.25
+      coupon.append(1)
+      ree.append("beef")
+      price.append(6.25)
+  elif sandwich_order == ("chicken"):
+      sandwich = 5.25
+      coupon.append(1)
+      price.append(5.25)
+      ree.append("chicken")
+  elif sandwich_order == ("tofu"):
+      sandwich = 5.75
+      coupon.append(1)
+      ree.append("tofu")
+      price.append(5.75)
+  print(sandwich_order + " " + str(sandwich))
+  # drink time wooooooooo
+  drink_order = input("Large drink(2.25) medium drink(1.75) small drink (1.00) or no?")
+  if drink_order == ("large"):
+      drink = 2.25
+      coupon.append(1)
+      price.append(2.25)
+      ree.append("Large drink")
+  elif drink_order == ("medium"):
+      drink = 1.75
+      coupon.append(1)
+      price.append(1.75)
+      ree.append("Medium drink")
+  elif drink_order == ("small"):
+      drink = 1.00
+      coupon.append(1)
+      price.append(1.00)
+      ree.append("Small drink")
+  elif drink_order == ("no"):
+      drink = 0.0
+  # fries woop woop
+  fry_order = input("large fries(2.00) medium fries(1.50) small fries(1.00)")
+  if fry_order == ("no"):
+      fries = 0.0
+  elif fry_order == ("medium"):
+      fries = 1.50
+      coupon.append(1)
+      price.append(1.50)
+      ree.append("Medium fries")
+  elif fry_order == ("small"):
+      megasize = input("would you like to megasize that?")
+      if megasize == ("yes"):
+          fries = 2.00
+          coupon.append(1)
+          price.append(2.00)
+          ree.append("Large fries")
+          print("succesfully MEGASIZED")
+      elif megasize == "no":
+          fries = 1.00
+          coupon.append(1)
+          price.append(1.00)
+          ree.append("Small fries")
+  elif fry_order == "large":
+      fries = 2.00
+      coupon.append(1)
+      price.append(2.00)
+      ree.append("Large fries")
+      # ketchup time
+  ketchup_packets = input("how many ketchup packets would you like? (0.25) each")
+  ketchup = (float(ketchup_packets) * 0.25)
+  price.append(ketchup)
+  ree.append(ketchup_packets + " ketchup packets")
+  final_order = input("Is that all? (yes or no)")
+  if final_order == "no":
+    memes()
+memes()
+total = sum(price)
+if sum(coupon) >= 2:
     total -= 1
     print("you get a discount")
 print("your order is " + str(ree))
